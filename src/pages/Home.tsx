0f0a5d6ce92020f96b056abcb2bf92de5a53d195
Vitinho <vitinho.dev@gmail.com>
2021-07-12 13:34:54 -0300

    desafio 2 concluido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+export interface EditTaskArgs {
+  taskId: number,
+  taskNewTitle: string
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    if (!newTaskTitle) return
+    if(!newTaskTitle) return
+
+    const findTask = tasks.find(( task ) => task.title === newTaskTitle)
+    if(findTask) return Alert.alert("Task já cadastrada", "Você não pode cadastrar uma task com o mesmo nome")
 
     const data = {
       id: new Date().getTime(),
@@ -34,9 +42,29 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks((oldState) => oldState.filter(
-      (task) => task.id !== id
-    ))
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+          style: "cancel"
+        },
+        { text: "Sim", onPress: () => setTasks((oldState) => oldState.filter((task) => task.id !== id))}
+      ]
+    )
+  }
+
+  function handleEditTask({ taskId, taskNewTitle }: EditTaskArgs) {
+    const updatedTasks = tasks.map((task) => ({...task}))
+
+    const editedTask = updatedTasks.find((task) => task.id === taskId)
+    
+    if(!editedTask) return
+    
+    editedTask.title = taskNewTitle
+
+    setTasks(updatedTasks)
   }
 
   return (
@@ -47,6 +75,7 @@ export function Home() {
 
       <TasksList 
         tasks={tasks} 
+        editTask={({taskId, taskNewTitle}) => handleEditTask({ taskId, taskNewTitle })}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
       />
